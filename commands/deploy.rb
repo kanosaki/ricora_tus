cad66fc267853fc57db66af7c13a3cd294584529
kanosaki <kanosaki@gmail.com>
2011-11-14 17:25:11 +0900

    Deploy command implemented and add some articles

--- commands/deploy.rb
+++ commands/deploy.rb
@@ -1,7 +1,7 @@
-
+require 'pp'
 usage       'deplpy'
 aliases     :dep
-summary     'Deploy pages to remote server'
+summary     'Compile and Deploy pages to remote server'
 description 'Homuhomu'
 
 flag  :h, :help, 'Show help for this command' do |value, cmd|
@@ -9,6 +9,54 @@ flag  :h, :help, 'Show help for this command' do |value, cmd|
   exit 0
 end
 
+required :t, :remote, "Specify remote uri"
+required :n, :name, "Specify remote name in config.yaml"
+
 run do |opts, args, cmd|
-  puts "Oops, not implemented yet."
+  Nanoc3::CLI::Commands::Compile.call(opts, args, cmd)
+  RsyncDeploy.call(opts, args, cmd)
+end
+
+class RsyncDeploy < Nanoc3::CLI::Command
+  def run
+    puts "Deploying site.."
+    puts "Target: #{target}"
+    permission_modify
+    exec_deploy
+    puts "Deploy complete"
+  end
+
+  def exec_deploy
+    system(create_deploy_command)
+  end
+  
+  def permission_modify
+    system("find #{output_root} -type d -exec chmod og+rx {} \\;")
+    system("find #{output_root} -type f -exec chmod og+r {} \\;")
+  end
+
+  def create_deploy_command
+    "rsync -avz -e ssh \"./#{output_root}/\" \"#{target}\""
+  end
+
+  def output_root
+    "./#{site.config[:output_dir]}/"
+  end
+  def target
+    target_from_args || target_from_config
+  end
+
+  protected
+  def target_from_args
+    options[:remote]
+  end
+
+  def target_from_config
+    cfg = site.config[:deploy]
+    name = options.has_key?(:confname) ? options[:confname] : :default
+    raise "deploy section wa not found in config.yaml" unless cfg
+    raise "The remote named #{name} was not found in config.yaml" unless cfg.has_key?(name)
+    raise "Invalid config file structure, 'dst' entry is required at /deploy/#{name}/dst in config.yaml" unless cfg[name].has_key?(:dst)
+    cfg[name][:dst]
+  end
 end
